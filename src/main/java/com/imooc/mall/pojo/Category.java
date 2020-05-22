43ed9623bd9cf8274452228003ea7efe763eb6d8
Ltyro <17816874829@163.com>
2020-05-22 16:37:45 +0800

    mall

---
+++ src/main/java/com/imooc/mall/pojo/Category.java
@@ -0,0 +1,75 @@
+package com.imooc.mall.pojo;
+
+import java.util.Date;
+
+public class Category {
+    private Integer id;
+
+    private Integer parentId;
+
+    private String name;
+
+    private Boolean status;
+
+    private int sortOrder;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Integer parentId) {
+        this.parentId = parentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public int getSortOrder() {
+        return sortOrder;
+    }
+
+    public void setSortOrder(int sortOrder) {
+        this.sortOrder = sortOrder;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
